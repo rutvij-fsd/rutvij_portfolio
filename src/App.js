bd94f1184196d2ca4ded2ce11d49f27c3bd5b00a
rutvij-fsd <rutvijpatel414@gmail.com>
2022-09-15 00:11:54 +0530

    Add Footer Section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Works from './Component/Works/Works';
 import Portfolio from './Component/Portfolio/Portfolio';
 import Testimonial from './Component/Testimonial/Testimonial';
 import Contact from './Component/Contact/Contact';
+import Footer from './Component/Footer/Footer';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
      <Portfolio/>
      <Testimonial/>
      <Contact/>
+     <Footer/>
 
     </div>
   );
