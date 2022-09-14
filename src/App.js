3f149096069bd937bb7ce79d71ab2ec7d65e6ae3
rutvij-fsd <rutvijpatel414@gmail.com>
2022-09-14 20:10:26 +0530

    Add Experience Section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Intro from './Component/Intro/Intro';
 import Services from './Component/Services/Services';
+import Experience from './Component/Experience/Experience';
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Intro/>
      <Services/>
+     <Experience/>
 
     </div>
   );
